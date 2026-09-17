ea7cfcac16ec240bd17bb2ea41fc21505ba39cee
Bhukya Akriti Sharanya <b.akritisharanya@gmail.com>
2026-09-17 22:54:50 +0530

    Add files via upload

---
+++ Rhombus.java
@@ -0,0 +1,28 @@
+package akriti505.java;
+
+import java.util.Scanner;
+public class Rhombus {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter number of rows: ");
+	        int n = sc.nextInt();
+
+	        for (int i = 1; i <= n; i++) {
+
+	            // Print spaces
+	            for (int j = 1; j <= n - i; j++) {
+	                System.out.print(" ");
+	            }
+
+	            // Print stars
+	            for (int j = 1; j <= n; j++) {
+	                System.out.print("* ");
+	            }
+
+	            System.out.println();
+	        }
+
+	        sc.close();
+	    }
+}	
